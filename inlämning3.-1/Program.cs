9808f4ebae5067716150724dea8baa0a956553ff
Pingvu <nhlpool27@gmail.com>
2023-10-17 14:21:57 +0200

    Add project files.

---
+++ inlämning3.-1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace uppgift
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hej, Hur gammal är du");
+
+            int svar = int.Parse(Console.ReadLine());
+
+            switch (svar)
+            {
+
+                case 16:
+                    Console.WriteLine("Du är tillräckligt gammal för att delta i tävlingen");
+                    break;
+
+                case 17:
+                    Console.WriteLine("Du är tillräckligt gammal för att delta i tävlingen");
+                    break;
+
+                case 18:
+                    Console.WriteLine("Du är tillräckligt gammal för att delta i tävlingen");
+                    break;
+                default:
+                    Console.WriteLine("Du får inte delta i tävlingen");
+
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
